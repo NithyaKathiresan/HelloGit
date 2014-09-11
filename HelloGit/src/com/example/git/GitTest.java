4312c1b25313d768ad5e65975b82f9b17dc531fe
NithyaKathiresan <nk@buildingapprovals.com>
2014-09-11 15:05:00 +0800

    Changed the output message- to test commit changes

--- HelloGit/src/com/example/git/GitTest.java
+++ HelloGit/src/com/example/git/GitTest.java
@@ -2,6 +2,6 @@ package com.example.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Git is cool");
 	  }
 }
